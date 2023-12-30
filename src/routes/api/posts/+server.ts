1fdf92be6a7d735504426d49cf0ae2c92321ef89
dangnguyenblackie <phucdang91thcsdl@gmail.com>
2023-12-30 22:45:04 +0700

    1.0.3-Add Simple GET METHOD for posts-Pending Review

--- src/routes/api/posts/+server.ts
+++ src/routes/api/posts/+server.ts
@@ -1,12 +1,32 @@
+import type { Post } from "$lib/types";
 import { json, type RequestHandler } from "@sveltejs/kit";
 
 async function getPosts(){
-    let posts = []
+    let posts: Post[] = []
     
-    const path = import.meta.glob('/src/posts/*.md')
+    const paths = import.meta.glob('/src/posts/*.md',{
+        eager: true
+    })
 
     // console.log(path)
-    
+    for (const path in paths)
+    {
+        // get SLUG by getting file name
+        const slug = path.split('/').at(-1)?.replace(".md",'');
+        
+        // get file
+        const file = paths[path];
+        
+        // get metadata from file 
+        const metadata = file.metadata;
+
+        // merge metadata and slug
+        const post: Post = {...metadata, slug};
+
+        post.published && posts.push(post);
+
+    }
+
     return posts
 }
 
