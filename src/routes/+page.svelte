8286c4fe232b9f6de5e6b56a658bb1ed2fbf0307
dangnguyenblackie <phucdang91thcsdl@gmail.com>
2023-12-31 00:42:56 +0700

    1.0.4-Display posts

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,4 +1,33 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script lang="ts">
+    import * as config from '$lib/config.js'
+    export let data;
+</script>
 
-# Hello
+<svelte:head>
+    <title>
+        {config.title}
+    </title>
+</svelte:head>
+
+<section>
+    <ul class="posts">
+        {#each data.posts as post}
+            <li class="post">
+                <a href={post.slug} class="title">{post.title}</a>
+                <p class="date">{post.date}</p>
+                <p class="description">{post.description}</p>
+            </li>
+        {/each}
+    </ul>
+</section>
+
+<style>
+    .posts{
+        display: grid;
+        gap: var(--size-7);
+    }
+
+    .post{
+        max-inline-size: var(--size-content-3);
+    }   
+</style>
